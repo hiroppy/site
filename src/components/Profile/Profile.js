013e69235f298bbf274402dc418190d2c7d01b66
Yuta Hiroto <git@about-hiroppy.com>
2018-07-15 10:08:59 +0900

    feat(Profile): add BizReach

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -25,12 +25,10 @@ const Profile = () => (
         <span className="transition">hello[at]hiroppy.me</span><br /><br />
         <span className="transition">Request for writing, speech, work etc...</span>
         <h2>My Career</h2>
-        <h3>Dwango</h3>
-        <span className="transition">2017/12 -</span>
-        <h3>Mercari</h3>
-        <span className="transition">2017/06 - 2017/12</span>
-        <h3>Dwango</h3>
-        <span className="transition">2015/04 - 2017/06</span>
+        <h3>BizReach: <span className="transition">2018/06 -</span></h3>
+        <h3>Dwango: <span className="transition">2017/12 -</span></h3>
+        <h3>Mercari: <span className="transition">2017/06 - 2017/12</span></h3>
+        <h3>Dwango: <span className="transition">2015/04 - 2017/06</span></h3>
       </div>
     </div>
   </Container>
