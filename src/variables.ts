109b7697968502b3bcd5a463e47ec70f550c4d98
Yuta Hiroto <git@about-hiroppy.com>
2019-02-27 14:14:07 +0900

    feat: add typescript entry point

---
+++ src/variables.ts
@@ -0,0 +1,13 @@
+export const mainColor = '#3498db';
+export const whiteColor = '#f5f5f5';
+export const blackColor = '#22292c';
+export const githubColor = '#767676';
+export const twitterColor = '#00aced';
+export const facebookColor = '#305097';
+export const hatenaColor = '#008fde';
+
+export const maxWidth = 1005;
+
+export const smartphoneSize = 500;
+export const tabletSize = 650;
+export const pcSize = 900;
