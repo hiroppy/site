2b8a761723a109cb70bc9498843879312758878c
Yuta Hiroto <git@about-hiroppy.com>
2019-02-27 14:36:00 +0900

    refactor: use styled-reset

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import ScrollReveal from 'scrollreveal';
 import styled, { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 import { Hero } from './Hero';
 import { Profile } from './Profile';
 import { GitHub } from './GitHub';
@@ -10,6 +11,7 @@ import font from '../../fonts/StratumNo1 Medium.ttf';
 import { mediaQueries } from '../utils/mediaQueries';
 
 const GlobalStyle = createGlobalStyle`
+  ${reset}
   @import url("https://fonts.googleapis.com/css?family=Encode+Sans+Semi+Condensed|Questrial");
 
   @font-face {
@@ -28,23 +30,31 @@ const GlobalStyle = createGlobalStyle`
 
   h1 {
     font-size: 3.6rem;
+    margin: 32px 0;
   }
 
   h2 {
     font-size: 3.2rem;
+    margin: 20px 0;
   }
 
   h3 {
     font-size: 2.4rem;
+    margin: 16px 0;
   }
 
   h4 {
     font-size: 2.0rem;
+    margin: 12px 0;
   }
 
   p, a {
     font-size: 2.0rem;
     font-weight: 100;
+  }
+
+  p {
+    margin: 12px 0;
   }
 
   a {
