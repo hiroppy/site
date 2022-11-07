f0e1a59c508c2d1565bec77d8144d2aefa823b41
Yuta Hiroto <git@hiroppy.me>
2022-11-08 08:37:10 +0900

    feat: add robots and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://hiroppy.me",
   integrations: [
     tailwind(),
     partytown({
@@ -11,5 +13,6 @@ export default defineConfig({
         forward: ["dataLayer.push"],
       },
     }),
+    sitemap(),
   ],
 });
