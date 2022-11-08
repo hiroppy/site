6c3086e531841432ffd9c5363df38ee116cc1f99
Yuta Hiroto <git@hiroppy.me>
2022-11-09 07:56:36 +0900

    fix: omit () from connpass title

--- scripts/talks.mjs
+++ scripts/talks.mjs
@@ -4,15 +4,22 @@ const data = await crawlSites("talks");
 const errors = [];
 
 // validate
-for (const slide of data) {
-  if (!slide.title) {
-    errors.push(slide.publishedAt);
+for (const talk of data) {
+  if (!talk.title) {
+    errors.push(talk.publishedAt);
   }
 }
 
 if (errors.length !== 0) {
   console.log(errors.join(","));
   process.exit(1);
+}
+
+// connpassのog:titleに付いている日付を消す
+for (const talk of data) {
+  if (talk.siteName) {
+    talk.siteName = talk.siteName.replace(/\(.+?\)$/, "").trim();
+  }
 }
 
 await generateData("talks", data);
