4cb21cb90af9df3229464134a4a92d4887f09a04
Yuta Hiroto <git@hiroppy.me>
2022-11-14 21:17:04 +0900

    fix: up

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -7,6 +7,18 @@ module.exports = {
     },
     assert: {
       preset: "lighthouse:no-pwa",
+      assertions: {
+        // for partytown
+        deprecations: "off",
+        // no js
+        "csp-xss": "off",
+        "render-blocking-resources": ["error", { maxLength: 2 }],
+        "uses-responsive-images": "off",
+        // for twitter card
+        "unused-javascript": "off",
+        // a blog page doesn't convert to webp
+        "uses-optimized-images": "off",
+      },
     },
   },
 };
