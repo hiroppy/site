d68d3a6fa76e2d9ab5d9aa4c1d8a2004954362a2
Yuta Hiroto <git@hiroppy.me>
2022-11-17 08:47:03 +0900

    test: vrt on linux

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,8 @@ import type { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
   webServer: {
-    command: "npm run preview",
+    // docker内でsharpを動かすのは難しいのでastro previewを使わない
+    command: "serve dist",
     port: 3000,
   },
 };
