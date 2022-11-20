04fb915814b86b99590f5b7d4f82a89faf770807
Yuta Hiroto <git@hiroppy.me>
2022-11-20 15:35:42 +0900

    test(vrt): allow parallel

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,7 @@
 import type { PlaywrightTestConfig } from "@playwright/test";
 
 const config: PlaywrightTestConfig = {
+  fullyParallel: true,
   webServer: {
     // docker内でsharpを動かすのは難しいのでastro previewを使わない
     command: "serve dist",
