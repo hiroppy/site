e105c892eb98d08c2e968018485d769c908cb885
Yuta Hiroto <git@hiroppy.me>
2023-05-08 09:59:22 +0900

    fix: up

--- .prettierrc.cjs
+++ .prettierrc.cjs
@@ -1,1 +1,12 @@
-module.exports = {};
+module.exports = {
+  // https://github.com/withastro/prettier-plugin-astro/blob/main/README.md#pnpm-support-1
+  plugins: [require.resolve("prettier-plugin-astro")],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
