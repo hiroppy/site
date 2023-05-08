f89225863c1f2bf0541ac823a6c01e784a4852bc
Yuta Hiroto <git@hiroppy.me>
2023-05-08 10:07:00 +0900

    fix: up

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,3 +1,5 @@
 module.exports = {
-  "*.{md,json,yml,astro,ts,js,cjs,mjs}": ["prettier --write"],
+  "*.{md,json,yml,astro,ts,js,cjs,mjs}": [
+    "prettier --write --plugin-search-dir=.",
+  ],
 };
