2625e16fd2b6078a77fa2f38267da9f829f5abe0
Yuta Hiroto <git@hiroppy.me>
2023-12-15 20:21:31 +0900

    refactor: lighthouse

--- .lighthouserc.cjs
+++ .lighthouserc.cjs
@@ -10,18 +10,12 @@ module.exports = {
     assert: {
       preset: "lighthouse:no-pwa",
       assertions: {
-        // TODO: remove
-        "non-composited-animations": "off",
-        // for partytown
-        deprecations: "off",
         // no js
         "csp-xss": "off",
-        "render-blocking-resources": ["error", { maxLength: 4 }],
         "uses-responsive-images": "off",
         // for twitter card
         "unused-javascript": "off",
         // for article page
-        "unsized-images": "off",
         "bf-cache": "off",
         // the default of astro image component
         "lcp-lazy-loaded": "off",
