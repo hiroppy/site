e4468985ba42b22a53d72d07bdc19ea67935c082
Yuta Hiroto <git@hiroppy.me>
2025-02-20 20:39:14 +0900

    test: update snapshot

--- src/constants.ts
+++ src/constants.ts
@@ -28,7 +28,8 @@ export const PAGE_LINKS = <const>[
 export const HATENA_BLOG_URL = "https://abouthiroppy.hatenablog.jp";
 export const HATENA_BLOG_ENTRY_URL = `${HATENA_BLOG_URL}/entry`;
 
-export const JOB_FORM = "https://forms.gle/8RWCDQzDDTnv3inX8";
+export const JOB_FORM =
+  "https://coder-penguin.com/#%E3%81%8A%E5%95%8F%E3%81%84%E5%90%88%E3%82%8F%E3%81%9B";
 
 export type PAGE_PATH = (typeof PAGE_LINKS)[number]["href"];
 export type MediaType = "articles" | "podcasts" | "achievements" | "talks";
